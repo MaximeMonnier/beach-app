c13ba55e5c8b300e7e5a839084edbd90e5936e3f
maxime monnier <maximemonnier39@gmail.com>
2024-03-11 22:23:01 +0100

    import gsap animation

--- frontend/src/pages/Login_Register.tsx
+++ frontend/src/pages/Login_Register.tsx
@@ -1,14 +1,26 @@
-import Register from '../components/Register'
-import Connexion from '../components/Connexion'
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import Register from '../components/Register';
+import Connexion from '../components/Connexion';
 
 function Login_Register() {
-    return (
-      <div className='container-form'>
-        <Register />
-        <div className="bar"></div>
-        <Connexion/>
-      </div>
-    )
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const elements = containerRef.current.children;
+    gsap.fromTo(elements, 
+      { y: 50, opacity: 0 }, 
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1 }
+    );
+  }, []);
+
+  return (
+    <div ref={containerRef} className='container-form'>
+      <Register />
+      <div className="bar"></div>
+      <Connexion/>
+    </div>
+  );
 }
 
-export default Login_Register
+export default Login_Register;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173, // Spécifiez le port si différent de 3000
-    // Vous pouvez également ajouter d'autres configurations de serveur ici selon vos besoins
+    port: 5173,
   },
 });
