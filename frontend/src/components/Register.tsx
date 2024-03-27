62d78cbcf72da3540898c74088621fd734ec4c48
maxime monnier <maximemonnier39@gmail.com>
2024-03-27 10:25:00 +0100

    test

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,6 +1,50 @@
+import React, { useState } from "react";
 import Logo from "../assets/logo_leubeach.webp";
 
 function Register() {
+  const [firstName, setFirstName] = useState("");
+  const [familyName, setFamilyName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dob, setDob] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const userData = {
+      first_name: firstName,
+      familly_name: familyName,
+      email: email,
+      password: password,
+      dob: dob,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const contentType = response.headers.get("content-type");
+      if (
+        !response.ok ||
+        !contentType ||
+        !contentType.includes("application/json")
+      ) {
+        throw new Error("Réponse non JSON ou erreur de requête");
+      }
+
+      const result = await response.json();
+
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -14,13 +58,14 @@ function Register() {
             </div>
 
             <div className="container-form-register">
-              <form action="">
+              <form onSubmit={handleSubmit}>
                 <div className="container-input">
                   <label>Nom</label>
                   <input
                     type="text"
                     placeholder="Votre nom"
-                    name="nom"
+                    name="familly_name"
+                    onChange={(e) => setFamilyName(e.target.value)}
                     required
                   />
                 </div>
@@ -30,7 +75,8 @@ function Register() {
                   <input
                     type="text"
                     placeholder="Votre prénom"
-                    name="prenom"
+                    name="first_name"
+                    onChange={(e) => setFirstName(e.target.value)}
                     required
                   />
                 </div>
@@ -41,6 +87,7 @@ function Register() {
                     type="email"
                     placeholder="Votre email"
                     name="email"
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                   />
                 </div>
@@ -51,36 +98,19 @@ function Register() {
                     type="password"
                     placeholder="Votre mot de passe"
                     name="password"
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                   />
                 </div>
 
                 <div className="container-input">
                   <label>Date de naissance</label>
-                  <input type="date" name="dateNaissance" required />
-                </div>
-
-                <div className="container-input">
-                  <label>Genre</label>
-                  <select className="value-register" name="genre" required>
-                    <option value="">Sélectionner...</option>
-                    <option value="femme">Femme</option>
-                    <option value="homme">Homme</option>
-                    <option value="autre">Autre</option>
-                  </select>
-                </div>
-
-                <div className="container-condition-register">
                   <input
-                    type="checkbox"
-                    id="conditions"
-                    name="conditions"
+                    type="date"
+                    name="dob"
                     required
+                    onChange={(e) => setDob(e.target.value)}
                   />
-                  <label htmlFor="conditions">
-                    J'accepte les conditions d'utilisation
-                  </label>
-                  {/* Intégration CAPTCHA ici */}
                 </div>
 
                 <div className="container-register-btn">
