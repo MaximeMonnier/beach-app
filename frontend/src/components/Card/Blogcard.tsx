3f8b479d1287ea538fd454d948c2b894a57418c7
maxime monnier <maximemonnier39@gmail.com>
2024-05-03 19:56:41 +0200

    front blog almost ok next step btn like and midleware permission action

--- frontend/src/components/Card/Blogcard.tsx
+++ frontend/src/components/Card/Blogcard.tsx
@@ -46,17 +46,19 @@ const Blogcard: React.FC<BlogcardProps> = ({
 
   const handleUpdate = () => {
     onUpdate({
-        title: editedTitle,
-        author: author,
-        content: editedContent,
-        onDelete: function (_id: string): void {
-            throw new Error("Function not implemented.");
-        },
-        onUpdate: function (_updatedArticle: Omit<BlogcardProps, "id" | "createdAt">): void {
-            throw new Error("Function not implemented.");
-        }
+      title: editedTitle,
+      author: author,
+      content: editedContent,
+      onDelete: function (_id: string): void {
+        throw new Error("Function not implemented.");
+      },
+      onUpdate: function (
+        _updatedArticle: Omit<BlogcardProps, "id" | "createdAt">
+      ): void {
+        throw new Error("Function not implemented.");
+      },
     });
-};
+  };
 
   return (
     <>
@@ -75,29 +77,34 @@ const Blogcard: React.FC<BlogcardProps> = ({
             <p>{formattedDate}</p>
           </div>
           <div className="w-full flex justify-end">
-            <button>
-              <i
-                className="fa-solid fa-pen text-color-3"
-                onClick={handleChange}
-              ></i>
+            <button
+              className="btn btn-outline border-none hover:bg-orange-600 "
+              onClick={handleChange}
+            >
+              <i className="fa-solid fa-pen text-black hover:text-color-1 dark:text-color-1"></i>
             </button>
-            <button onClick={() => onDelete(id)}>
-              <i className="fa-solid fa-xmark text-color-6 ml-2"></i>
+            <button
+              className="btn btn-outline border-none hover:bg-red-600 "
+              onClick={() => onDelete(id)}
+            >
+              <i className="fa-solid fa-xmark text-black font-bold hover:text-color-1 dark:text-color-1"></i>
             </button>
+
+            <button></button>
           </div>
         </div>
         <div className="w-full border-2 border-color-6 mb-4 rounded-xl dark:border-color-5"></div>
         <div className="w-full">
           {transform ? (
-            <div className="mb-4">
+            <div className="mb-4 ">
               <input
                 type="text"
                 placeholder="Ajouter votre titre"
                 name="title"
                 id="title"
                 value={editedTitle}
-                onChange={handleTitleChange }
-                className="input input-bordered w-full h-10  bg-bg-1 border border-color-3"
+                onChange={handleTitleChange}
+                className="input input-bordered w-full h-10  bg-bg-1 border border-color-3 dark:bg-bg-3"
               />
             </div>
           ) : (
@@ -106,7 +113,7 @@ const Blogcard: React.FC<BlogcardProps> = ({
           {transform ? (
             <div className="mb-4 flex flex-col">
               <textarea
-                className="textarea textarea-bordered bg-bg-1 border border-color-3"
+                className="textarea textarea-bordered bg-bg-1 border border-color-3 dark:bg-bg-3"
                 placeholder="Bio"
                 name="content"
                 id="content"
@@ -114,7 +121,7 @@ const Blogcard: React.FC<BlogcardProps> = ({
                 onChange={handleContentChange}
               ></textarea>
               <button
-                className="btn btn-neutral w-20 flex m-auto mt-2 bg-bg-2 text-color-1 hover:bg-bg-6"
+                className="btn btn-neutral w-20 flex m-auto mt-2 bg-bg-2 text-color-1 hover:bg-bg-6 dark:bg-bg-3"
                 onClick={handleUpdate}
               >
                 Valider
@@ -123,6 +130,25 @@ const Blogcard: React.FC<BlogcardProps> = ({
           ) : (
             <p>{content}</p>
           )}
+        </div>
+        <div>
+          <button className="btn">
+            Button
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </>
